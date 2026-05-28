3eecc469e125b6f5175d796656766110aa206713
yangshipu <3301319743@qq.com>
2026-05-28 15:54:15 +0800

    add detach

--- CLIP+ResNet+CoOp/config.py
+++ CLIP+ResNet+CoOp/config.py
@@ -6,27 +6,27 @@ class Config:
     # --- 数据集路径 (改为 CSV 模式) ---
     TRAIN_CSV = r'研究拓展/dataset/train_subset.csv'  # 你的训练集 CSV 路径
     VAL_CSV = r'研究拓展/dataset/val_subset.csv'      # 你的验证集 CSV 路径
-
+    
     # --- 实验输出配置 ---
-    SAVE_DIR = r'研究拓展/CLIP+ResNet+CoOp/checkpoints'             # 模型权重保存目录
-    LOG_FILE = r'研究拓展/CLIP+ResNet+CoOp/runs/training_log.txt'        # 训练日志保存路径
-
+    SAVE_DIR = r'研究拓展/CLIP+ResNet+CoOp/checkpoints/have_detach'             # 模型权重保存目录
+    LOG_FILE = r'研究拓展/CLIP+ResNet+CoOp/runs/have_detach/training_log.txt'        # 训练日志保存路径
+    
     # --- 图像预处理 ---
-    IMAGE_SIZE = 224
-
+    IMAGE_SIZE = 224  
+    
     # --- MRT-43K 类别与文本提示 ---
     CLASSES = ['bedrock', 'gravel', 'looserock', 'norulerock', 'sand']
     NUM_CLASSES = len(CLASSES)
-
+    
     # --- 训练超参数 ---
     BATCH_SIZE = 32
     EPOCHS = 50
-    LR_CNN = 1e-4
-    LR_CLIP = 1e-6
+    LR_CNN = 1e-4      
+    LR_CLIP = 1e-6     
     WEIGHT_DECAY = 1e-4
-
+    
     # --- 损失函数权重 ---
-    LAMBDA_1 = 1.0
+    LAMBDA_1 = 1.0     
     LAMBDA_2 = 1.0
 
     # ---CoOp所需配置--- #
